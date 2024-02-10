d52aa001ea9a9bb289e440c0edb0189c4b168350
snyata <p4rlx-news@pm.me>
2024-02-11 01:04:53 +1030

    CF AI Gateway

--- libs/shared/ts-api-template/dist/routes/chat-routes.js
+++
@@ -1,5 +0,0 @@
-// Chat Routes
-import { Router } from "express";
-const chatRoutes = Router();
-export default chatRoutes;
-//# sourceMappingURL=chat-routes.js.map
--- libs/shared/ts-api-template/dist/utils/constants.js
+++
@@ -1,5 +0,0 @@
-// CONSTANT VALUES
-import path from "path";
-export const COOKIE_NAME = "auth_token";
-export const ENV_PATH = path.join('./', '..', '.env');
-//# sourceMappingURL=constants.js.map
